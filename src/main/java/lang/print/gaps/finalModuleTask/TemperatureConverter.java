5a530d8465550963e2172a80db610f577807447c
NANO <File changed>
2022-07-18 00:17:06 +0600

    6 submission

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -3,7 +3,7 @@ package lang.print.gaps.finalModuleTask;
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
 
-        int Fahrenheit;
+        float Fahrenheit;
         Fahrenheit =((temperatureCelsius*9)/5)+32;
         System.out.println( Fahrenheit);
     }
