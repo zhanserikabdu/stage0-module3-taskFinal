058c72873b98148a0e5cc9636291455332ea8c14
NANO <File changed>
2022-07-17 23:34:06 +0600

    1 submission

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,17 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int rev = 0; // reversed number
+        int rem;   // remainder
+
+        while(number>0){
+
+            rem = number%10;
+            rev = (rev*10) + rem;
+            number = number/10;
+        }
+
+        return rev;
     }
+
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,6 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-
-    }
+        if(number >0){
+      System.out.println("true");
+    }else{
+            System.out.println("false");
+        }
 }
